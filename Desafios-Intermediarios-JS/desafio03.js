9027fb154081bff1bf4c77d3bac2af6dccd10c8a
cadumeneses <cadumeneses>
2022-06-21 11:18:16 -0300

    desafios intermediarios JS - dio

---
+++ Desafios-Intermediarios-JS/desafio03.js
@@ -0,0 +1,12 @@
+let n = 1;
+let min = 150;
+let max = 180;
+
+		let altura, conta = 0;
+		for (let x=0 ; x<n ; x++) {
+            altura = 175
+		               
+		if(altura >= min && altura <= max )
+			conta++;
+		}
+		console.log( conta );
